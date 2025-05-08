72757e2a3d97e241aeef67c35789ffb91fa3eb19
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2025-05-08 22:58:34 +0530

    Refactor Todo application to enhance add and get functionality, improve user prompts, and ensure proper content handling.

--- C++/practice/apps/Todo/src/main.cpp
+++ C++/practice/apps/Todo/src/main.cpp
@@ -11,8 +11,8 @@
 enum class Choice {
     Add = 1,
     Get = 2,
-    Delete = 4,
     Update = 3,
+    Delete = 4,
 };
 
 std::string generateRandomID(size_t length = 12) {
@@ -40,35 +40,89 @@ class Todo {
     bool completed = false;
 
    public:
-    Todo() {
-        this->id = generateRandomID();
+    void addTodo(std::string content) {
+        id = generateRandomID();
+        this->content = content;
 
-        std::cout << "Enter todo ";
-        std::getline(std::cin >> std::ws, this->content);
+        std::cout << ">>>>>>>>>> TODO IS ADDED SUCCESSFULLY <<<<<<<<<<<<<" << std::endl;
     }
 
-    void getTodo() const {
+    void getTodoProps() const {
         std::cout << "Content " << content << std::endl;
-        std::cout << "Is Completed " << completed << std::endl;
+        std::cout << "Is Completed " << completed << std::endl << std::endl;
     }
+
+    string getTodoContents() const { return content; }
 };
 
 class TodoManger {
    private:
     std::vector<Todo> todos;
 
-   public:
-    void addTodo(Todo todo) {
-        todos.push_back(todo);
-        std::cout << "Todo is added successfully" << std::endl;
+    std::string getContentFromUser() {
+        std::string content;
+        std::cout << "Enter you todo content to add it. " << std::endl;
+        std::getline(std::cin >> std::ws, content);
+
+        if (content.length() == 1) {
+            return NULL;
+        }
+
+        return content;
     }
 
-    void getTodo() {
-        std::cout << "------------------ LIST OF TODOS -----------------" << std::endl;
-        for (const Todo& todo : todos) {
-            todo.getTodo();
+   public:
+    void addTodo() {
+        std::string content = getContentFromUser();
+
+        if (content != NULL) {
+            Todo todo;
+            todo.addTodo(content);
+
+            todos.push_back(todo);
+        } else {
         }
     }
+
+    void getAllTodo() {
+        std::cout << "------------------ LIST OF TODOS -----------------" << std::endl;
+        int counter = 1;
+        for (const Todo& todo : todos) {
+            std::cout << "Sr No. " << counter << std::endl;
+            todo.getTodoProps();
+
+            counter += 1;
+        }
+    }
+
+    void updateTodo() {
+        std::cout << "------------------ CHOOSE BETWEEN THEM ⬇️ -----------------" << std::endl;
+        int choice;
+
+        int counter = 1;
+        for (const Todo& todo : todos) {
+            std::cout << counter << ". " << todo.getTodoContents() << std::endl;
+        }
+
+        std::cout << "Enter you choice: ";
+
+        if (choice > todos.size()) {
+            std::cout << "⚠️ INVALID REQUEST OUT OF RANGE..." << std::endl;
+        } else {
+            std::string content;
+            std::cout << "Enter you todo content to add it. " << std::endl;
+            std::getline(std::cin >> std::ws, content);
+
+            if (content.length() > 0) {
+                Todo todo;
+                todo.addTodo(content);
+
+                todos.push_back(todo);
+            }
+        }
+    }
+
+    void removeTodo() {}
 };
 
 int main() {
@@ -77,22 +131,39 @@ int main() {
 
     while (true) {
         int choice;
+        std::cout << "1. To add new todo. \n 2. Update Todo. \n 3. Update existing todo. \n 4. "
+                     "Delete todo."
+                  << std::endl;
         std::cout << "Enter you choice: ";
         std::cin >> choice;
         Choice userChoice = static_cast<Choice>(choice);
 
         switch (userChoice) {
             case (Choice::Add): {
-                std::cout << "---------------- ADDING A NEW TODO ------------------" << std::endl;
-                Todo todo;
+                std::cout << "******************** ADDING A NEW TODO **********************"
+                          << std::endl;
 
-                todoManger->addTodo(todo);
+                todoManger->addTodo();
                 break;
             }
             case (Choice::Get): {
-                std::cout << "--------------------- GET TODO -------------------" << std::endl;
+                std::cout << "******************** GET ALL TODOS **********************"
+                          << std::endl;
 
                 todoManger->getTodo();
+                break;
+            }
+            case (Choice::Update): {
+                std::cout << "******************** UPDATE EXISTING TODO **********************"
+                          << std::endl;
+
+                todoManger->updateTodo();
+                break;
+            }
+            case (Choice::Delete): {
+                std::cout << "******************** DELETE EXISTING TODO **********************"
+                          << std::endl;
+
                 break;
             }
             default: {
