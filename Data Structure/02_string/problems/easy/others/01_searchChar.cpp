a1ab9bad4df785aa1fbedf9946c3cc66d01c0505
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2025-07-16 23:05:42 +0530

    Refactor searchChar functions to improve clarity and remove unnecessary output

--- Data Structure/02_string/problems/easy/others/01_searchChar.cpp
+++ Data Structure/02_string/problems/easy/others/01_searchChar.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 
 int searchChar(std::string s, char ch) {
-    std::cout << s << std::endl;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ch) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+// Using in-build function
+int searchCharS(std::string s, char ch) {
+    size_t idx = s.find(ch);
+
+    // When these functions ('find', 'rfind', 'find_last_of', 'find_first_of') not found return
+    // std::string:npos
+    if (idx != std::string::npos) {
+        return idx;
+    }
 
     return -1;
 }
@@ -11,7 +28,12 @@ int main() {
     char s1 = 'l';
     char s2 = 'n';
 
-    searchChar(name);
+    std::cout << "Finding " << s1 << " in name " << searchChar(name, s1) << "\n";
+    std::cout << "Finding " << s2 << " in name " << searchChar(name, s2) << "\n";
+
+    std::cout << "<==========> Second Method <============>" << "\n";
+    std::cout << "Finding " << s1 << " in name " << searchCharS(name, s1) << "\n";
+    std::cout << "Finding " << s2 << " in name " << searchCharS(name, s2) << "\n";
 
     return 0;
 }
