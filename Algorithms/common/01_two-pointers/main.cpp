34361175969cb935337053cd6ea19b1bc8f4226f
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2025-07-23 23:32:31 +0530

    Add initial implementations and .gitkeep files for various algorithms and data structures
    - Created directories and .gitkeep files for KMP, Robin-Karp, Tries, Suffix Array, Sorting, Searching, Prefix Sum, Hashing, Greedy, Monotonic Stack, Kadane's Algorithm, Heap/Priority Queue, Bit Manipulation, and common techniques like Two Pointers and Sliding Window.
    - Implemented a basic Two Pointers technique example in main.cpp.
    - Added a check palindrome example in the Data Structure section.

---
+++ Algorithms/common/01_two-pointers/main.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    <======================> Two Pointers <======================>
+
+    ◉ Two Pointers technique is very effective to solve these problems such as "two sum with sorted
+        array", "closest two sum", "three sum", "four sum", "trapping rain water" and many other
+        popular interview questions.
+    ◉ So in two pointer technique as name suggest they use two pointers (indices) to traverse on the
+        data structure especially on "ARRAY", "STRING", "LINKED-LIST". The pointers often represent
+        (left indices, right indices) moving towards each other on in same direction are manipulate
+        based on certain conditions, often reduce the time complexity from o(n²) to o(n).
+
+
+    <======================> Example <======================>
+    ◉ Example 1:
+                    arr = [10, 20, 35, 50], target = 70
+                    left = 0, right = 4 - 1 = 3;
+
+                    ◉ Step 1: left = 0, right = 3
+                            ◉ arr[left] = 10, arr[right] = 50 = 60 ==> 60 >> left += 1, Why "left +=
+                                1" not "right -= 1" because it is sorted arr if sum of both is
+                                smaller than target means arr[0] num is smaller to achieve target.
+                    ◉ Step 2: left = 1, right = 3
+                            ◉ arr[left] = 20, arr[right] = 50 = 70, We found are pair which is
+                                {20,50}
+
+*/
+
+void twoSum(vector<int>& num, int target) {
+    int left = 0;
+    int right = num.size() - 1;
+
+    while (left < right) {
+        int sum = num[left] + num[right];
+
+        // Sum match to target
+        if (sum == target) {
+            cout << num[left] << " - " << num[right] << endl;
+            break;
+        } else if (sum < target) {
+            // In the sorted array always arr[right] will be greater than arr[left].
+            // If sum of both is smaller than target which means arr[left] is smaller. than left++;
+            left++;
+        } else {
+            // In the sorted array always arr[right] will be greater than arr[left].
+            // If sum of both is greater than target which means arr[right] is larger. than right--;
+            right--;
+        }
+    }
+}
+
+void reverseString(string& a) {
+    int i = 0;
+    int j = a.size() - 1;
+
+    while (i < j) {
+        char temp = a[i];
+
+        a[i] = a[j];
+        a[j] = temp;
+
+        i++;
+        j--;
+    }
+}
+
+int main() {
+    string s = "abcdefg";
+    vector<int> num1 = {10, 20, 35, 50};
+
+    twoSum(num1, 70);
+    reverseString(s);
+
+    cout << s << endl;
+
+    return 0;
+}
--- Data Structure/02_string/string.md
+++ Data Structure/02_string/string.md
@@ -289,3 +289,89 @@
         return 0;
     }
     ```
+
+7. [**Check Palindrome:**](./problems/easy/others/06_check_palindrome.cpp) Palindrome is a technique used to check whether original string is same to reverse string.
+
+    ```cpp
+    #include <bits/stdc++.h>
+    using namespace std;
+    
+    /*
+        <====================> CHECK PALINDROME <======================>
+    
+        ◉ Palindrome is a technique used to check whether original string is same to reverse string.
+        ◉ Example: a = "abcdef", rev_a = "fedcba" ==> False
+        ◉ Example: a = "aba", rev_a = "aba" ==> True.
+    */
+    
+    // Check palindrome using two pointers technique. tc = o(n), sc = o(1)
+    bool checkPalindrome1(string a) {
+        int i = 0;
+        int n = a.size() - 1;
+    
+        while (i < n) {
+            if (a[i] != a[n])
+                return false;
+    
+            i++;
+            n--;
+        }
+    
+        return true;
+    }
+    
+    // Optimization on above one instead of using two variable we using single one.
+    bool checkPalindrome2(string a) {
+        int n = a.size();
+    
+        for (int i = 0; i < n / 2; i++) {
+            if (a[i] != a[(n - 1) - i])
+                return false;
+        }
+    
+        return true;
+    }
+    
+    // Using recursion to check palindrome or not.
+    bool checkPalindromeUsingRec(string a, int left, int right) {
+        if (left > right) {
+            return true;
+        }
+    
+        if (a[left] != a[right])
+            return false;
+    
+        return checkPalindromeUsingRec(a, left + 1, right - 1);
+    }
+
+    // Function to check if a string is a palindrome
+    int isPalindrome(string& s) {
+        // If reverse string is equal to given string,
+        // then it is palindrome.
+        return s == string(s.rbegin(), s.rend()) ? 1 : 0;
+    }
+    
+    int main() {
+        string a = "aba";
+        string b = "abcdef";
+        string c = "abba";
+    
+        cout << checkPalindrome1(a) << endl;
+        cout << checkPalindrome1(b) << endl;
+        cout << checkPalindrome1(c) << endl;
+    
+        cout << "<==============> Second Approach <================>" << endl;
+    
+        cout << checkPalindrome2(a) << endl;
+        cout << checkPalindrome2(b) << endl;
+        cout << checkPalindrome2(c) << endl;
+    
+        cout << "<==============> Using Recursion <================>" << endl;
+    
+        cout << checkPalindromeUsingRec(a, 0, a.length() - 1) << endl;
+        cout << checkPalindromeUsingRec(b, 0, b.length() - 1) << endl;
+        cout << checkPalindromeUsingRec(c, 0, c.length() - 1) << endl;
+    
+        return 0;
+    }
+    ```
