a6b3e91e185af554745f290ce833fc9fb2e67e3c
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-08 21:08:43 +0530

    Add min-max and sorting/searching utility functions with examples

---
+++ C++/05_function/dsa-utils/min-max.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+std::vector<int> nums = {2, 6, 1, 52, 89, 500};
+
+// 👉 Min/Max: Return either "max" value or "min" value from two.
+void usingMinMax() {
+    std::cout << "--> Using minmax: " << std::endl;
+
+    std::cout << "Min value between 44, 90: " << std::min(44, 90) << std::endl;
+    std::cout << "Max Value between 4252, 52: " << std::max(4252, 52) << std::endl;
+}
+
+// 👉 min_element: Returns an iterator to the min_element between the given range
+void usingMinElement() {
+    std::cout << std::endl << "--> Using min_element: " << std::endl;
+
+    auto minElement = std::min_element(nums.begin(), nums.end());
+
+    std::cout << "Min element on nums is: " << *minElement << std::endl;
+}
+
+// 👉 max_element: Return an iterator for the max_element between the given range
+void usingMaxElement() {
+    std::cout << std::endl << "--> Using max_element: " << std::endl;
+
+    std::vector<int>::iterator maxElement = std::max_element(nums.begin(), nums.end());
+    std::cout << "Max element on nums is: " << *maxElement << std::endl;
+}
+
+// 👉 minmax: Return a pair for min and max value from two value, first represents min and second represents max
+void using_minmax() {
+    std::cout << std::endl << "--> Using minmax: " << std::endl;
+
+    std::pair<int, int> minmaxValue = std::minmax(700, 200);
+    std::cout << "Min value is: " << minmaxValue.first << " -- "
+              << "Max value is: " << minmaxValue.second << std::endl;
+}
+
+// 👉 clamp: Keep the value between given range, used for clean boundary problems.
+// 👉 If value become greater than high then directly return max_high value instead of "given"
+// 👉 If value become smaller than low then directly return low_value instead of "given value"
+// 👉 If value is within the range then it will return "value".
+void usingClamp() {
+    std::cout << std::endl << "--> Using clamp: " << std::endl;
+
+    std::cout << "55, Keep the value between 0 to 60: " << std::clamp(55, 0, 60) << std::endl;
+    std::cout << "100, Keep the value between 0 to 60: " << std::clamp(100, 0, 60) << std::endl;
+    std::cout << "-200, Keep the value between 0 to 60: " << std::clamp(-200, 0, 60) << std::endl;
+}
+
+int main() {
+    usingMinMax();
+    usingMinElement();
+    usingMaxElement();
+    using_minmax();
+    usingClamp();
+
+    return 0;
+}
---
+++ C++/05_function/dsa-utils/sorting_searching.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+static std::vector<int> nums = {6, 7, 2, 9, 25, 68, 190};
+
+void print(std::vector<int> vec) {
+    std::cout << "Elements are: ";
+    for (const int& n : vec) {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+}
+
+// 👉 Binary Search: It's a build-in for doing "binary search" and it returns either true/false.
+void usingBinarySearch() {
+    std::cout << "Is 190 exists on nums: " << std::binary_search(nums.begin(), nums.end(), 190)
+              << std::endl;
+    std::cout << "Is 55 exists on nums: " << std::binary_search(nums.begin(), nums.end(), 55)
+              << std::endl;
+}
+
+// 👉 Upper bound: Return an iterator for first element greater than "value"
+void usingUpperBound() {
+    std::cout << std::endl << "--> Upper Bound: " << std::endl;
+
+    auto upperBound1 = std::upper_bound(nums.begin(), nums.end(), 2);
+    std::vector<int>::iterator upperBound2 = std::upper_bound(nums.begin(), nums.end(), 68);
+
+    std::cout << "First element greater than 68: " << *upperBound1 << std::endl;
+    std::cout << "First element greater than 2: " << *upperBound2 << std::endl;
+}
+
+// 👉 Lower bound: Return an iterator for first element lower than "value"
+void usingLowerBound() {
+    std::cout << std::endl << "--> Lower Bound: " << std::endl;
+
+    auto lowerBound1 = std::lower_bound(nums.begin(), nums.end(), 7);
+    auto lowerBound2 = std::lower_bound(nums.begin(), nums.end(), 190);
+
+    std::cout << "First element greater than 7: " << *lowerBound1 << std::endl;
+    std::cout << "First element greater than 190: " << *lowerBound2 << std::endl;
+}
+
+int main() {
+    usingBinarySearch();
+    usingUpperBound();
+    usingLowerBound();
+
+    return 0;
+}
