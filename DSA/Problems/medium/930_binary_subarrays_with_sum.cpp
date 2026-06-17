ae7bd142892ebe315a7910e2b74d54fa8a5e03f7
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-17 10:59:05 +0530

    Enhance linked list implementation: refactor Stack and Queue classes, improve memory management, and add main function for demonstratiodn

--- DSA/Problems/medium/930_binary_subarrays_with_sum.cpp
+++ DSA/Problems/medium/930_binary_subarrays_with_sum.cpp
@@ -5,8 +5,8 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-// 🟡 Brute force appraoch:
-// 👉 [ Remember ]: You were put the "sum" at the end, which was wrong because what happend if we got 2 -> goal at start, absolutely we were miss that.
+// 🟡 Brute force approach:
+// 👉 [ Remember ]: You were put the "sum" at the end, which was wrong because what happen if we got 2 -> goal at start, absolutely we were miss that.
 int countSubarraySumEqualGoalBruteForce(const vector<int>& nums, const int goal) {
     const int size = static_cast<int>(nums.size());
 
@@ -32,13 +32,13 @@ int countSubarraySumEqualGoalBruteForce(const vector<int>& nums, const int goal)
 
 /* 🟡 Optimized approach:
  👉 [ Remember ]:
-                🔸 First thing It won't going to solve using simple sliding window, the problem with this approach is we'll going to miss lots of subarray who's sum is equal to goal.
-                🔸 So instead of finding subarray who's sum is exactly equal to goal, find two things:
-                    1. Find Subarrays who sum is less than equal to goal, I know it will going to consider everything. Suppose goal is 2 then it will consider 0, 1, 2.
+                🔸 First thing It won't go to solve using simple sliding window, the problem with this approach is we'll go to miss lots of subarray whose sum is equal to goal.
+                🔸 So instead of finding subarray whose sum is exactly equal to goal, find two things:
+                    1. Find Subarrays who sum is less than equal to goal, I know it will go to consider everything. Suppose goal is 2 then it will consider 0, 1, 2.
                     2. Find Subarrays who sum is less than equal to "goal - 1", then it will give everything from 0 and 1, if Goal is 2.
-                    3. Now the final part, substract all goal from 0 to 2 with goal from 0 to 1, means Now we'll got the subarrays who's goal is 2.
+                    3. Now the final part, subtract all goal from 0 to 2 with goal from 0 to 1, means Now we'll get the subarrays whose goal is 2.
 */
-int countSubarraySumEqualGoal(const vector<int>& nums, const int goal) {
+int countSubarraySumEqualGoals(const vector<int>& nums, const int goal) {
     const int size = static_cast<int>(nums.size());
 
     auto countSubarray = [=](const int g) -> int {
@@ -69,7 +69,7 @@ int countSubarraySumEqualGoal(const vector<int>& nums, const int goal) {
 
 int main() {
     const vector<int> nums = {0, 0, 0, 0, 0};
-    cout << "Total subarrays are: " << countSubarraySumEqualGoal(nums, 0) << endl;
+    cout << "Total subarrays are: " << countSubarraySumEqualGoals(nums, 0) << endl;
 
     return 0;
 }
