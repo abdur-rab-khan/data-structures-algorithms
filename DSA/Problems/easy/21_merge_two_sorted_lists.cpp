0b42561d3841427c97a752896c9fccf88b7a1ebf
Abdur Rab Khan <abdurrabkhan3263@gmail.com>
2026-06-24 11:24:37 +0530

    Add forward_list implementation: create forward_list.cpp and update list.cpp to include forward_list header

--- DSA/Problems/easy/21_merge_two_sorted_lists.cpp
+++ DSA/Problems/easy/21_merge_two_sorted_lists.cpp
@@ -7,19 +7,31 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::to_string;
+
+struct Node {
+    int   value;
+    Node* next;
+    explicit Node(const int value) : value(value), next(nullptr) {};
+};
 
 class SinglyLinkedList {
-   private:
-    struct Node {
-        int   value;
-        Node* next;
-        explicit Node(const int value) : value(value), next(nullptr) {};
-    };
+   public:
     Node* tail_;
     Node* head_;
 
-   public:
     SinglyLinkedList() : tail_(nullptr), head_(nullptr) {}
+    explicit SinglyLinkedList(Node* head = nullptr, Node* tail = nullptr)
+        : tail_(tail), head_(head) {}
+
+    ~SinglyLinkedList() {
+        const Node* currentNode = head_;
+        while (currentNode != nullptr) {
+            const Node* nextNode = currentNode->next;
+            delete currentNode;
+            currentNode = nextNode;
+        }
+    }
 
     void append(const int value) {
         const auto newNode = new Node(value);
@@ -33,12 +45,56 @@ class SinglyLinkedList {
         }
     }
 
-    string toString() const {
+    SinglyLinkedList operator+(const SinglyLinkedList& list2) const {
+        Node* head1 = head_;
+        Node* head2 = list2.head_;
+
+        if (head1 == nullptr) {
+            return SinglyLinkedList(head2, list2.tail_);
+        }
+
+        if (head2 == nullptr) {
+            return SinglyLinkedList(head1, tail_);
+        }
+
+        Node* ptr   = head1;
+        Node* tail_ = head1;
+
+        if (head1->value > head2->value) {
+            ptr   = head2;
+            tail_ = head2;
+            head2 = head2->next;
+        } else {
+            head1 = head1->next;
+        }
+
+        while (head1 && head2) {
+            if (head1->value > head2->value) {
+                tail_->next = head2;
+                tail_       = tail_->next;
+                head2       = head2->next;
+            } else {
+                tail_->next = head1;
+                tail_       = tail_->next;
+                head1       = head1->next;
+            }
+        }
+
+        if (head1 != nullptr) {
+            tail_->next = head1;
+        } else {
+            tail_->next = head2;
+        }
+
+        return SinglyLinkedList(ptr, tail_);
+    }
+
+    string toString() {
         string nodes = "";
 
         const Node* currentNode = head_;
         while (currentNode != nullptr) {
-            nodes += std::to_string(currentNode->value) + " ";
+            nodes += to_string(currentNode->value) + " ";
             currentNode = currentNode->next;
         }
 
@@ -46,11 +102,48 @@ class SinglyLinkedList {
     }
 };
 
-typedef SinglyLinkedList::Node List;
+typedef Node* List;
 
-auto mergeTwoList(List list1, List list2) {
-    return list1;
-}
+// List mergeTwoList(List list1, List list2) {
+//     if (list1 == nullptr) {
+//         return list2;
+//     }
+//
+//     if (list2 == nullptr) {
+//         return list1;
+//     }
+//
+//     List ptr   = list1;
+//     List tail_ = list1;
+//
+//     if (list1->value > list2->value) {
+//         ptr   = list2;
+//         tail_ = list2;
+//         list2 = list2->next;
+//     } else {
+//         list1 = list1->next;
+//     }
+//
+//     while (list1 && list2) {
+//         if (list1->value > list2->value) {
+//             tail_->next = list2;
+//             tail_       = tail_->next;
+//             list2       = list2->next;
+//         } else {
+//             tail_->next = list1;
+//             tail_       = tail_->next;
+//             list1       = list1->next;
+//         }
+//     }
+//
+//     if (list1 != nullptr) {
+//         tail_->next = list1;
+//     } else {
+//         tail_->next = list2;
+//     }
+//
+//     return ptr;
+// }
 
 int main() {
     const auto list1 = new SinglyLinkedList();
@@ -65,6 +158,14 @@ int main() {
     list2->append(1);
     list2->append(3);
     list2->append(4);
+
+    List mergedList = mergeTwoList(list1->head_, list2->head_);
+
+    cout << "Merged elements are: ";
+    while (mergedList != nullptr) {
+        cout << mergedList->value << " ";
+        mergedList = mergedList->next;
+    }
 
     delete list1;
     delete list2;
---
+++ DSA/Problems/medium/61_rotate_list.cpp
@@ -0,0 +1,105 @@
+//
+// Created by abdur on 23-06-2026.
+//
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+
+class SinglyLinkedList {
+   private:
+    struct Node {
+        int   value;
+        Node* next;
+        explicit Node(const int value) : value(value), next(nullptr) {};
+    };
+    Node* tail_;
+    Node* head_;
+
+   public:
+    SinglyLinkedList() : tail_(nullptr), head_(nullptr) {}
+    ~SinglyLinkedList() {
+        const Node* currentNode = head_;
+        while (currentNode != nullptr) {
+            const Node* nextNode = head_->next;
+            delete currentNode;
+            currentNode = nextNode;
+        }
+    }
+
+    void append(const int value) {
+        const auto newNode = new Node(value);
+        if (head_ == nullptr) {
+            head_ = newNode;
+            tail_ = newNode;
+        } else {
+            tail_->next = newNode;
+            tail_       = tail_->next;
+        }
+    }
+
+    void rotate(const int k) {
+        if (head_ == nullptr) {
+            return;
+        }
+
+        // Step 1. Find the size and tail of the list
+        int   size = 1;  // head_ is non-null, So minimum size is 1
+        Node* tail = head_;
+        while (tail->next != nullptr) {
+            tail = tail->next;
+            ++size;
+        }
+
+        // Step 2. Compute effective rotation and find the new tail
+        const int effectiveRotation = ((k % size) + size) % size;
+        if (effectiveRotation == 0) {
+            return;
+        }
+
+        const int stepsToNewTail = size - effectiveRotation - 1;
+        Node*     newTail        = head_;
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail->next;
+        }
+
+        // Step 3. Rewrite pointers -- attach old tail to old head, update new head
+        Node* newHead = newTail->next;
+        newTail->next = nullptr;  // Breaking the chain
+        tail->next    = head_;
+        head_         = newHead;
+    }
+
+    string toString() const {
+        string nodes;
+
+        const Node* currentNode = head_;
+        while (currentNode != nullptr) {
+            nodes += to_string(currentNode->value) + " ";
+            currentNode = currentNode->next;
+        }
+
+        return nodes.empty() ? "(EMPTY)" : nodes;
+    }
+};
+
+int main() {
+    const auto singlyLinkedList = new SinglyLinkedList();
+
+    singlyLinkedList->append(1);
+    singlyLinkedList->append(2);
+    singlyLinkedList->append(3);
+    singlyLinkedList->append(4);
+    singlyLinkedList->append(5);
+
+    cout << "Elements are: " << singlyLinkedList->toString() << endl;
+
+    singlyLinkedList->rotate(2);
+
+    cout << "Elements are: " << singlyLinkedList->toString() << endl;
+
+    delete singlyLinkedList;
+    return 0;
+}
