874e1a6baf603c6b17dbfe2bb97246ce2db51622
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2026-07-15 13:05:27 +0530

    Document STL sequence containers and learned about forward_list and list

--- C++/01_data-type/stl/01_sequence_container/forward_list.cpp
+++ C++/01_data-type/stl/01_sequence_container/forward_list.cpp
@@ -1,10 +1,75 @@
-#include <iostream>
-#include <list>
+/*
+ +-----------------------------------------------------------------------------------------------------------------------------------------------------------------+
+ |                                                                        Forward List                                                                             |
+ +-----------------------------------------------------------------------------------------------------------------------------------------------------------------+
+ |                                                                                                                                                                 |
+ | * "std::forward_list" is a container used to store element in a sequence, it's usually implemented using "Singly-LinkedList". Good thing about                  |
+ |   "std::forward_list" (singly linked list) is we can insert or remove element as constant time complexity "o(1)".                                               |
+ |                                                                                                                                                                 |
+ +-----------------------------------------------------------------------------------------------------------------------------------------------------------------+
+ |                                                                                                                                                                 |
+ | * Element Access:                                                                                                                                               |
+ |   1. front(): Returns first element                                                                                                                             |
+ |   3. find(start_it, end_it, target): Used to access random element from the list and it returns an iterator                                                     |
+ |                                                                                                                                                                 |
+ | * Capacity:                                                                                                                                                     |
+ |   1. empty(): Returns the bool whether list is empty or not                                                                                                     |
+ |                                                                                                                                                                 |
+ | * Modifiers:                                                                                                                                                    |
+ |   1. insert_after(iterator, element): Insert element after the iterator                                                                                         |
+ |   2. insert_range_after(iterator, container_compatible_range): Insert list of elements                                                                          |
+ |                                                                                                                                                                 |
+ |   1. clear(): Remove all the elements from the list                                                                                                             |
+ |   2. erase_after(element): Remove particular elements                                                                                                           |
+ |                                                                                                                                                                 |
+ |   2. push_front():                                                                                                                                              |
+ |   2. pop_front():                                                                                                                                               |
+ |                                                                                                                                                                 |
+ | * Operations:                                                                                                                                                   |
+ |   1. .merge(list): Merge two lists                                                                                                                              |
+ |   1. .sort(list): Sort list                                                                                                                                     |
+ |   1. .unique(list): Remove duplicate elements                                                                                                                   |
+ |                                                                                                                                                                 |
+ +-----------------------------------------------------------------------------------------------------------------------------------------------------------------+
+*/
 
-using std::cout;
-using std::endl;
-using std::list;
+#include <algorithm>
+#include <forward_list>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void printElements(const forward_list<int>& nums) {
+    cout << "Elements are: ";
+    for (const int& num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
 
 int main() {
+    forward_list<int> singlyLinkedList;
+
+    singlyLinkedList.push_front(5);
+    singlyLinkedList.push_front(4);
+    singlyLinkedList.push_front(3);
+    singlyLinkedList.push_front(2);
+    singlyLinkedList.push_front(1);
+
+    printElements(singlyLinkedList);
+
+    // Random push via given iterator
+    auto it = find(singlyLinkedList.begin(), singlyLinkedList.end(), 3);
+    singlyLinkedList.insert_after(it, 33);
+
+    printElements(singlyLinkedList);
+
+    vector<int> nums    = {11, 22, 33, 44, 55};
+    auto        startIt = singlyLinkedList.before_begin();
+    singlyLinkedList.insert_range_after(startIt, nums);
+
+    printElements(singlyLinkedList);
+
     return 0;
 }
