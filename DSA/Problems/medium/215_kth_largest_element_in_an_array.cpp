4eb7d97e35d77a8d011255cfd85ea9d1dae4fa25
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2026-07-15 10:46:43 +0530

    Add quickselect solution for kth largest element

--- DSA/Problems/medium/215_kth_largest_element_in_an_array.cpp
+++ DSA/Problems/medium/215_kth_largest_element_in_an_array.cpp
@@ -82,13 +82,58 @@ void quickSort(std::vector<int>& numbers, int k, int left, int right) {
     }
 }
 
+int partition(vector<int>& nums, int left, int right) {
+    random_device                 rd;
+    uniform_int_distribution<int> dist(left, right);
+
+    int pivotPoint   = dist(rd);
+    int pivotElement = nums[pivotPoint];
+    swap(nums[right], nums[pivotPoint]);
+
+    int leftIdx = left;
+    for (int rightIdx = left; rightIdx < right; rightIdx++) {
+        if (nums[rightIdx] > pivotElement) {
+            swap(nums[rightIdx], nums[leftIdx]);
+            leftIdx++;
+        }
+    }
+
+    swap(nums[leftIdx], nums[right]);
+    return leftIdx;
+}
+
+int quickSelect(vector<int>& nums, int left, int right, int k) {
+    if (left == right)
+        return nums[left];
+
+    int partitionIdx = partition(nums, left, right);
+
+    if (partitionIdx == k - 1) {
+        return nums[k - 1];
+    } else if (partitionIdx > k - 1) {
+        return quickSelect(nums, left, partitionIdx - 1, k);
+    } else {
+        return quickSelect(nums, partitionIdx + 1, right, k);
+    }
+}
+
 int main() {
-    int k1 = 4;
+    int         k    = 2;
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+
+    cout << "Kth largest element is: " << quickSelect(nums, 0, static_cast<int>(nums.size() - 1), k)
+         << endl;
+
+    return 0;
+}
+
+int main() {
+    int              k1       = 4;
     std::vector<int> numbers1 = {3, 2, 3, 2, 3, 1, 2, 4, 5, 5, 6};
     quickSort(numbers1, k1, 0, numbers1.size() - 1);
     std::cout << "Kth element of the sorted array is: " << numbers1[k1 - 1] << std::endl;
 
-    int k2 = 2;
+    int              k2       = 2;
     std::vector<int> numbers2 = {3, 2, 1, 5, 6, 4};
     quickSort(numbers2, k2, 0, numbers2.size() - 1);
     std::cout << "Kth element of the sorted array is: " << numbers2[k2 - 1] << std::endl;
