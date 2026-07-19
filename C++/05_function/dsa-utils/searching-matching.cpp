bd953e6c71d7d279abaf3b297b29a99f7dde843f
Abdur Rab Khan <abdurrabkhan222@gmail.com>
2026-07-19 10:37:04 +0530

    Remove unused files and added cmake on both apps

--- C++/05_function/dsa-utils/searching-matching.cpp
+++ C++/05_function/dsa-utils/searching-matching.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
-#include <iterator>
 #include <vector>
 
 using std::count;
@@ -17,70 +16,73 @@ vector<int> nums = {4, 20, 81, 500, 2, 1, 52, 1};
 
 // 👉 count: Return how many times "val" appear within the range
 void usingCount() {
-    cout << "--> Using Count: " << endl;
+  cout << "--> Using Count: " << endl;
 
-    int numberOfTwoAppears  = count(nums.begin(), nums.end(), 1);  // 1 --> 2
-    int numberOfFourAppears = count(nums.begin(), nums.end(), 4);  // 4 --> 1
+  int numberOfTwoAppears = count(nums.begin(), nums.end(), 1);  // 1 --> 2
+  int numberOfFourAppears = count(nums.begin(), nums.end(), 4); // 4 --> 1
 
-    cout << "Number of times 1 appears: " << numberOfTwoAppears << endl;
-    cout << "Number of times 4 appears: " << numberOfFourAppears << endl;
+  cout << "Number of times 1 appears: " << numberOfTwoAppears << endl;
+  cout << "Number of times 4 appears: " << numberOfFourAppears << endl;
 }
 
-// 👉 count_if: It's similar to "count", but we can pass a function to count element by condition
+// 👉 count_if: It's similar to "count", but we can pass a function to count
+// element by condition
 void usingCountIf() {
-    cout << endl << "--> Using Count-If: " << endl;
+  cout << endl << "--> Using Count-If: " << endl;
 
-    auto                  isEven = [](int i) { return i % 2 == 0; };
-    function<bool(int i)> isOdd  = [](int i) { return i % 2 != 0; };
+  auto isEven = [](int i) { return i % 2 == 0; };
+  function<bool(int i)> isOdd = [](int i) { return i % 2 != 0; };
 
-    int oddCounts  = count_if(nums.begin(), nums.end(), isOdd);
-    int evenCounts = count_if(nums.begin(), nums.end(), isEven);
+  int oddCounts = count_if(nums.begin(), nums.end(), isOdd);
+  int evenCounts = count_if(nums.begin(), nums.end(), isEven);
 
-    cout << "Number of odds are: " << oddCounts << endl;
-    cout << "Number of evens are: " << evenCounts << endl;
+  cout << "Number of odds are: " << oddCounts << endl;
+  cout << "Number of evens are: " << evenCounts << endl;
 }
 
-// 👉 find: Return an iterator of first element if it matched, otherwise return end()
+// 👉 find: Return an iterator of first element if it matched, otherwise return
+// end()
 void usingFind() {
-    cout << endl << "--> Using Count-If: " << endl;
+  cout << endl << "--> Using Count-If: " << endl;
 
-    vector<int>::iterator isFourExists = find(nums.begin(), nums.end(), 4);
-    vector<int>::iterator isFiveExits  = find(nums.begin(), nums.end(), 5);
+  vector<int>::iterator isFourExists = find(nums.begin(), nums.end(), 4);
+  vector<int>::iterator isFiveExits = find(nums.begin(), nums.end(), 5);
 
-    if (isFourExists == nums.end()) {
-        cout << "Four isn't exist" << endl;
-    } else {
-        cout << "The number four is exists: " << *isFourExists << endl;
-    }
+  if (isFourExists == nums.end()) {
+    cout << "Four isn't exist" << endl;
+  } else {
+    cout << "The number four is exists: " << *isFourExists << endl;
+  }
 
-    if (isFiveExits == nums.end()) {
-        cout << "Five isn't exist" << endl;
-    } else {
-        cout << "The number five exists: " << *isFiveExits << endl;
-    }
+  if (isFiveExits == nums.end()) {
+    cout << "Five isn't exist" << endl;
+  } else {
+    cout << "The number five exists: " << *isFiveExits << endl;
+  }
 }
 
-// 👉 find_if: Return an iterator of first matched element using given function, otherwise return end
+// 👉 find_if: Return an iterator of first matched element using given function,
+// otherwise return end
 void usingFindIf() {
-    cout << endl << "--> Using Count-If: " << endl;
+  cout << endl << "--> Using Count-If: " << endl;
 
-    auto isEven = [](int i) { return i % 2 == 0; };
+  auto isEven = [](int i) { return i % 2 == 0; };
 
-    vector<int>::iterator firstEven = find_if(nums.begin(), nums.end(), isEven);
+  vector<int>::iterator firstEven = find_if(nums.begin(), nums.end(), isEven);
 
-    if (firstEven == nums.end()) {
-        cout << "No even exists" << endl;
-    } else {
-        cout << "First even is found: " << *firstEven << endl;
-    }
+  if (firstEven == nums.end()) {
+    cout << "No even exists" << endl;
+  } else {
+    cout << "First even is found: " << *firstEven << endl;
+  }
 }
 
 int main() {
-    usingCount();
-    usingCountIf();
+  usingCount();
+  usingCountIf();
 
-    usingFind();
-    usingFindIf();
+  usingFind();
+  usingFindIf();
 
-    return 0;
+  return 0;
 }
--- C++/fun-with-cpp/compiler_and_linking/log.cpp
+++ C++/fun-with-cpp/compiler_and_linking/log.cpp
@@ -3,11 +3,14 @@
 
 /*
  * [ Learn Extern ]:
- *                  "extern" tells the compiler that the variable is defined elsewhere, not in this file but in another file I promise that you will get from the linker
+ *        "extern" tells the compiler that the variable/function is defined
+ * somewhere, not in this file but in another file I promise that you will get
+ * from the linker
  */
 extern int globalCounter;
 
 void log(std::string message) {
-    std::cout << "In log function, globalCounter is:: " << globalCounter++ << std::endl;
-    std::cout << "Log message is:: " << message << std::endl;
+  std::cout << "In log function, globalCounter is:: " << globalCounter++
+            << std::endl;
+  std::cout << "Log message is:: " << message << std::endl;
 }
